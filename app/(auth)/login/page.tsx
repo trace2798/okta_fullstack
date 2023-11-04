560af5fd224f6ff84c7f7460ca97e2f555f216e2
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-11-04 13:39:24 +0530

    added back button on home page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,4 +1,6 @@
 import AuthForm from "@/components/auth-form";
+import { Button } from "@/components/ui/button";
+import { ChevronLeft } from "lucide-react";
 import { FC } from "react";
 
 interface pageProps {}
@@ -6,6 +8,13 @@ interface pageProps {}
 const page: FC<pageProps> = ({}) => {
   return (
     <>
+      <a href="/">
+        <Button variant="ghost" className="mt-3">
+          {" "}
+          <ChevronLeft /> Back Home
+        </Button>
+      </a>
+
       <div className="h-screen flex justify-center items-center">
         <AuthForm />
       </div>
